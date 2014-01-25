4273e910e22ed0c1bc239996f08c569c08da40b1
Vittorio Papandrea <papandreavittorio@gmail.com>
2014-01-24 19:20:59 -0500

    basic split by , and add to arraylist.

--- src/org/team2168/subsystems/Vision.java
+++ src/org/team2168/subsystems/Vision.java
@@ -2,28 +2,34 @@ package org.team2168.subsystems;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class Vision {
 	
+	static ArrayList<String> data = new ArrayList<String>();
+	
 	void getTCPPacket(String ip, int port) throws IOException
 	{
+		
+			//TODO Need to edit to 
 	
-		while(true)
-		{
 			Socket clientSocket = new Socket(ip, port);
-			//Send to server
 			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 			outToServer.writeBytes("1");	
 			String sentence;
 			String modifiedSentence;
-			//Recieve from server
 			BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
 			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			sentence = inFromUser.readLine();
 			modifiedSentence = inFromServer.readLine();
 			System.out.println("FROM SERVER: " + modifiedSentence);
+			
+			for(String d : modifiedSentence.split(","))
+			{
+				data.add(d);
+			}
 			clientSocket.close();
-		}
+	
 	}
 	
 }
