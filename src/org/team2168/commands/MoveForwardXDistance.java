39503941381373d88290ee84549515697eeab367
inkinc <736f6c@gmail.com>
2014-02-05 20:43:21 -0500

    added a quick process to correct the angle of the robot while driving forward, which may or may not work.

--- src/org/team2168/commands/MoveForwardXDistance.java
+++ src/org/team2168/commands/MoveForwardXDistance.java
@@ -7,12 +7,14 @@ public class MoveForwardXDistance extends CommandBase{
 	double distance;
 	double endDistance;
 	boolean finished;
+	double angle;
 	
 	public MoveForwardXDistance(double distance)
 	{
 		this.distance = distance;
 		endDistance = drivetrain.getAveragedEncoderDistance() + distance;
 		drivetrain = new Drivetrain();
+		angle = drivetrain.getGyroAngle();
 	}
 
 	protected void end() {
@@ -20,23 +22,36 @@ public class MoveForwardXDistance extends CommandBase{
 	}
 	
 	protected void execute() {
-		//TODO set the margin of error 
+		//TODO set the margin of error
+		
+		double rightSpeed = 1;
+		double leftSpeed = 1;
 		double currentDistance = drivetrain.getAveragedEncoderDistance();
 		
+		//make the left/right motors go less fast, to correct angle
+		if (drivetrain.getGyroAngle() < angle)
+		{
+			rightSpeed = 0.75;
+		}
+		else if (drivetrain.getGyroAngle() > angle)
+		{
+			leftSpeed = 0.75;
+		}
+		
 		//check if the robot is within the margin of error (1)
-		if (Math.abs(endDistance - currentDistance) < 1) {
+		if (Math.abs(endDistance - currentDistance) < 1)
+		{
 			drivetrain.drive(0,0);
 			finished = true;
 		}
-		if(currentDistance < endDistance)
+		else if(currentDistance < endDistance)
 		{
-			drivetrain.drive(1,1);
+			drivetrain.drive(leftSpeed,rightSpeed);
 		}
-		else if (currentDistance > endDistance)
+		else if(currentDistance > endDistance)
 		{
-			drivetrain.drive(-1,-1);
+			drivetrain.drive(-leftSpeed,-rightSpeed);
 		}
-		
 	}
 
 	protected void initialize()
