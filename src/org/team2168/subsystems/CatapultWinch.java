d7f5fa91074c295f4d8e6c059f6785222258af18
James <james@team2168.org>
2014-02-08 21:08:30 -0500

    Update CatapultWinch.java
    removed unused import

--- src/org/team2168/subsystems/CatapultWinch.java
+++ src/org/team2168/subsystems/CatapultWinch.java
@@ -1,8 +1,6 @@
-
 package org.team2168.subsystems;
 
 import org.team2168.RobotMap;
-import org.team2168.commands.DrivetrainWithJoystick;
 import org.team2168.commands.WinchWithJoystick;
 import org.team2168.utils.MomentaryDoubleSolenoid;
 import edu.wpi.first.wpilibj.DigitalInput;
