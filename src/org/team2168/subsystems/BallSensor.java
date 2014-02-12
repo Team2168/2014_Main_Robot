fecdfe318995b113dfd1d5ebc0f22a80a3774bd0
James <james@team2168.org>
2014-02-11 23:27:22 -0500

    Gave class a public getInstance method to work along side the   private constructor. Some style fixes. Comments.

--- src/org/team2168/subsystems/BallSensor.java
+++ src/org/team2168/subsystems/BallSensor.java
@@ -4,25 +4,39 @@ import org.team2168.RobotMap;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.AnalogChannel;
 
-
 public class BallSensor extends Subsystem {
-	
+
 	private AnalogChannel ballSensorVoltage;
-	
+	private static BallSensor instance = null;
+
 	/**
 	 * Gets the voltage of the sensor
 	 */
-	private BallSensor(){
+	private BallSensor() {
 		ballSensorVoltage = new AnalogChannel(RobotMap.ballSensorPort.getInt());
 	}
 	
-	public double getBallSensorVoltage() {
-				return ballSensorVoltage.getVoltage();
+	/**
+	 * Returns the static instance of this subsystem.
+	 * @return the instance of this subsystem.
+	 */
+	public static BallSensor getInstance() {
+		if (instance == null) {
+			instance = new BallSensor();
+		}
+		return instance;
 	}
-	
+
+	/**
+	 * Get the ball sensor voltage.
+	 * @return The voltage read from the ball sensor. 0.0 to 5.0
+	 */
+	public double getBallSensorVoltage() {
+		return ballSensorVoltage.getVoltage();
+	}
+
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
+		//No default command
 	}
 
 }
