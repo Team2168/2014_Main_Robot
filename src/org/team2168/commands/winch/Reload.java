4b53c1e9d538ff1aa5cb5a6bac97b69852893f6d
James <james@team2168.org>
2014-03-12 10:39:42 -0400

    Update Reload.java
    closes #77

--- src/org/team2168/commands/winch/Reload.java
+++ src/org/team2168/commands/winch/Reload.java
@@ -10,6 +10,9 @@ public class Reload extends CommandGroup {
 		//engage the winch drum
 		addSequential(new RetractWinchDogGear());
 		
+		//Retract the tusks in preparation for intaking next ball
+		addSequential(new TusksTrussShotPosition());
+		
 		//retract the catapult all the way
 		addSequential(new RetractWinchUntilLowered(
 				RobotMap.retractWinchSpeed.getDouble()));
