f96541e8b7537df947b9a175ef45e2f41824f4df
James <james@team2168.org>
2014-03-19 03:00:11 -0400

    Modified two ball
    Should operate quicker, drive fwd after firing, and use the sensors for ball settling.

--- src/org/team2168/commands/auto/Right_RightHotGoal_2Ball.java
+++ src/org/team2168/commands/auto/Right_RightHotGoal_2Ball.java
@@ -1,51 +1,62 @@
 package org.team2168.commands.auto;
 
 import org.team2168.RobotMap;
-import org.team2168.commands.Sleep;
 import org.team2168.commands.drivetrain.AutoDriveXDistance;
 import org.team2168.commands.intake.IntakeDown;
 import org.team2168.commands.intake.IntakeDriveMotor;
-import org.team2168.commands.intake.IntakeSingleBallAndSettle;
+import org.team2168.commands.intake.IntakeSingleBall;
+import org.team2168.commands.intake.IntakeUp;
+import org.team2168.commands.tapper.DisengageTappers;
+import org.team2168.commands.tapper.EngageTappers;
 import org.team2168.commands.tusks.TusksLongShotPosition;
-import org.team2168.commands.vision.WaitForLeftHot;
-import org.team2168.commands.vision.WaitForRightHot;
 import org.team2168.commands.winch.Fire;
 import org.team2168.commands.winch.FireAndReload;
 import org.team2168.commands.winch.Reload;
+import org.team2168.commands.winch.WaitUntilBallSettled;
 import org.team2168.commands.winch.WaitUntilFired;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitForChildren;
 
 public class Right_RightHotGoal_2Ball extends CommandGroup {
 
-
 	public Right_RightHotGoal_2Ball() {
-
 		// set tusks to long shot
 		addParallel(new TusksLongShotPosition());
 
-		//move intake out of way, spin wheel to keep ball in robot
+		// move intake out of way, spin wheel to keep ball in robot
 		addParallel(new IntakeDriveMotor(-0.25));
 		addSequential(new IntakeDown(), 1);
 		addParallel(new IntakeDriveMotor(0.0));
+		addSequential(new EngageTappers());
 		
-		//wait for ball to settle
-		addSequential(new Sleep(), RobotMap.autoBallSettleTime.getDouble());
+		// wait for ball to settle
+		addSequential(new WaitUntilBallSettled(),
+				RobotMap.autoBallSettleTime.getDouble());
 
-		//First ball
+		// First ball
 		addSequential(new FireAndReload());
 
-		//intake second ball
-		addSequential(new IntakeSingleBallAndSettle(),3);
+		// intake second ball
+		addSequential(new IntakeSingleBall(), 3.0);
+		addSequential(new EngageTappers());
 
-		//set tusks
+		// set tusks
 		addParallel(new TusksLongShotPosition());
-		
-		//wait for ball to settle
-		addSequential(new Sleep(), RobotMap.autoBallSettleTime.getDouble());
-		
-		//Second Ball
+
+		// wait for ball to settle
+		addSequential(new WaitUntilBallSettled(),
+				RobotMap.autoBallSettleTime.getDouble());
+
+		// Second Ball
 		addSequential(new Fire());
+		addSequential(new WaitUntilFired());
+		
+		//drive fwd for pts
+		addSequential(new AutoDriveXDistance(RobotMap.autoDriveDistance.getDouble()));
+		
+		//Get ready for the match 
+		addParallel(new DisengageTappers());
+		addParallel(new IntakeUp());
+		addParallel(new Reload());
 	}
 }
